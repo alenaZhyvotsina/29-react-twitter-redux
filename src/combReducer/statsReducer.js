abdfb227e9ba95fb64eec09b7c53600d11b1ec27
Helen Zhivotina <helen_zh@live.ru>
2020-11-07 21:38:01 +0200

    Combine Reducers upd

--- src/combReducer/statsReducer.js
+++ src/combReducer/statsReducer.js
@@ -2,7 +2,6 @@ import {CHANGE_STATS} from "../actions/StatsActions";
 import {initialStateStats} from "./initialState";
 
 export default function statsReducer(state = initialStateStats, action){
-    //console.log('statsReducer ', state.stats, '++', state.followers, action.type);
     const stats = state.stats;
     switch (action.type){
         case CHANGE_STATS:
@@ -12,15 +11,4 @@ export default function statsReducer(state = initialStateStats, action){
         default:
             return state;
     }
-     /*
-    switch (action.type){
-        case CHANGE_STATS:
-            const res = state[action.fieldType] + action.increment;
-            state[action.fieldType] = res < 0 ? 0 : res;
-            return state;
-        default:
-            return state;
-    }
-
-      */
 }
